92b10c49b22d9f28ad2a010312d658a96ed9ec3c
Mohamed Adel <mohamedadelelsaid46@gmail.com>
2024-04-23 22:38:31 +0200

    Implement network service using Alamofire

--- News-VIPER/Services/NetworkService.swift
+++ News-VIPER/Services/NetworkService.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import Alamofire
 
-final class NetworkService {
+protocol NetworkServiceProtocol {
+    func requestCNNNews(completion: @escaping (Result<NewsResponse, Error>) -> Void)
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    
+    func requestCNNNews(completion: @escaping (Result<NewsResponse, Error>) -> Void ) {
+        
+        guard let url = URL(string: AppConstants.newsURL) else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        AF.request(request).validate().responseDecodable(queue: .global()) { (response: DataResponse<NewsResponse,AFError>) in
+            switch response.result {
+            case .success(let newsResponse):
+                completion(.success(newsResponse))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
     
 }
